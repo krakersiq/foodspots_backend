bbba35ae0c2996f2c6416131c18a86fe3709b55c
Antonia <anrybaczyk@gmail.com>
2025-07-03 17:03:03 +0200

    add create

--- routes.js
+++ routes.js
@@ -1,12 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Foodspot = require("./models/foodspots");
 /* const Member = require('./models/members'); */
-const Foodspot = require('./models/foodspots');
+
+//get all foodspots Auslesen
+router.get("/foodspots", async (req, res) => {
+  const allFoodspots = await Foodspot.find();
+  console.log(allFoodspots);
+  res.send(allFoodspots);
+});
+
+// post one foodspot Eintragen
+router.post("/foodspots", async (req, res) => {
+  const newFoodspot = new Foodspot({
+    name: req.body.name,
+    stadt: req.body.stadt,
+    kueche: req.body.kueche,
+    bewertung_geschmack: req.body.bewertung_geschmack,
+    bewertung_preis: req.body.bewertung_preis,
+    bewertung_ambiente: req.body.bewertung_ambiente,
+    kommentar: req.body.kommentar,
+  });
+  await newFoodspot.save();
+  res.send(newFoodspot);
+});
 
 // eine GET-Anfrage
-router.get('/fiw', async(req, res) => {
-
-    res.send({ message: "Hello FIW!" });
+router.get("/fiw", async (req, res) => {
+  res.send({ message: "Hello FIW!" });
 });
 
 /* // get all members
@@ -16,11 +37,23 @@ router.get('/members', async(req, res) => {
     res.send(allMembers);
 }); */
 
-//get all foodspots
-router.get('/foodspots', async(req, res) => {
-    const allFoodspots = await Foodspot.find();
-    console.log(allFoodspots);
-    res.send(allFoodspots);
-});
+/* router.delete('/foodspots', async(req, res) => {
+    const foodspotId = req.query.id;
+    console.log(foodspotId);
+    if (!foodspotId) {
+        return res.status(400).send({ message: "Foodspot ID is required" });
+    }
+    
+    try {
+        const deletedFoodspot = await Foodspot.findByIdAndDelete(foodspotId);
+        if (!deletedFoodspot) {
+            return res.status(404).send({ message: "Foodspot not found" });
+        }
+        res.send({ message: "Foodspot deleted successfully", foodspot: deletedFoodspot });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "An error occurred while deleting the foodspot" });
+    }
+} */
 
 module.exports = router;
